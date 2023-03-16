e3ee771b5d8d0af061f2a5dbdd8248796884085d
Kbusisiwe <kbusisiwe404@gmail.com>
2023-03-16 11:28:54 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - My first C programming with ALX
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
