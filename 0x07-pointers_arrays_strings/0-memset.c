476120f1f67a52f3c9a29b8f01f1a6d3ebd0c460
Kbusisiwe <kbusisiwe404@gmail.com>
2023-04-03 07:32:07 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,18 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * _memset - 
+ * _memset - Entry point
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: 0 (Success)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
